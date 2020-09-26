4745eb09f6fbb96f0717a8e297414644fea9cd9a
henWel <henokwel@outlook.com>
2020-09-26 23:00:58 +0200

    Updates

---
+++ precache-manifest.d3a829873e048b3339d45aa13cfeba07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583e4aecad1736be699c6952f7bf15c6",
+    "url": "/langquiz/index.html"
+  },
+  {
+    "revision": "9745e88de0723defff62",
+    "url": "/langquiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d18f9fdff6fad2d2359",
+    "url": "/langquiz/static/js/2.87ded822.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/langquiz/static/js/2.87ded822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9745e88de0723defff62",
+    "url": "/langquiz/static/js/main.139e3c60.chunk.js"
+  },
+  {
+    "revision": "3ecda9b0e0aca0eabe30",
+    "url": "/langquiz/static/js/runtime-main.b387acd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/langquiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
